a9a8f2457255373018548919093f7a0877960fd5
strandbergryan <strandbergryan@gmail.com>
2023-10-02 17:06:14 -0600

    Created boiler and linked docs

---
+++ script/script.js
@@ -0,0 +1,12 @@
+alert("javascript is linked")
+
+class Tamagotchi {
+    constructor (name) {
+        this.name = name;
+        this.hunger = 10;
+        this.sleepiness = 10;
+        this.boredom = 10;
+        this.age = 0;
+        this.alive = true; 
+    }
+}
