043c53b5a99c8126ec1ff7da00fea13ab1f2d257
strandbergryan <strandbergryan@gmail.com>
2023-10-03 22:01:39 -0600

    buttons function with class

--- script/script.js
+++ script/script.js
@@ -1,17 +1,92 @@
 class Tamagotchi {
-    constructor (name) {
+    constructor(name) {
         this.name = name;
-        this.hunger = 10;
-        this.sleepiness = 10;
-        this.boredom = 10;
+        this.hunger = 5;
+        this.sleepiness = 5;
+        this.boredom = 5;
         this.age = 0;
-        this.alive = true; 
+        this.alive = true;
+
+        this.ageIntervalID = setInterval(() => {
+            this.incrementAge();
+            displayMetrics();
+        }, 60000);
+    }
+
+
+    incrementAge() {
+        this.age++;
+    }
+
+    feed() {
+        if (this.hunger > 0 ) {
+            this.hunger--;
+        }
+        displayMetrics();
+    }
+
+    play() {
+        if (this.boredom > 0 ) {
+            this.boredom--;
+        }
+        displayMetrics();
+    }
+
+    sleep() {
+        if (this.sleepiness > 0 ) {
+            this.sleepiness--;
+        }
+        displayMetrics();
     }
 }
 
-function promptForName() {
-    const petName = prompt("Please give your pet a name")
-    const newPet = new Tamagotchi(petName);
+let pet;
+
+function CreateTamagotchi(name) {
+    return new Tamagotchi (name)
 }
 
-window.onload = promptForName; 
+function promptForName() {
+    const petName = prompt("Please give your pet a name");
+    if (petName) {
+        document.getElementById("user-name").textContent = `${petName}`;
+        pet = new Tamagotchi(petName);
+        displayMetrics();
+    }
+}
+
+function displayMetrics() {
+    const petMetrics = document.querySelector(".petMetrics");
+    const currentPet = pet;
+
+    if (currentPet) {
+        petMetrics.innerHTML =
+    `<ul>
+        <li>Hunger: ${pet.hunger}</li>
+        <li>Boredom: ${pet.boredom}</li>
+        <li>Sleepiness: ${pet.sleepiness}</li>
+        <li>Age: ${pet.age}</li>
+    </ul>
+    `;
+    }
+}
+
+function feedPet() {
+    if (pet) {
+        pet.feed();
+    }
+}
+
+function playPet() {
+    if (pet) {
+        pet.play();
+    }
+}
+
+function sleepPet() {
+    if (pet) {
+        pet.sleep();
+    }
+}
+
+window.onload = promptForName(); 
